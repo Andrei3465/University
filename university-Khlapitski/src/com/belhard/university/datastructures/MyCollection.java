e0cfea11e770ced3197a4bbbfaf8b690d7ac41b3
Andrei Khlapitski <a.khlapitski@gmail.com>
2022-03-29 01:33:59 +0300

    Add method size to My Collection

--- university-Khlapitski/src/com/belhard/university/datastructures/MyCollection.java
+++ university-Khlapitski/src/com/belhard/university/datastructures/MyCollection.java
@@ -2,10 +2,16 @@ package com.belhard.university.datastructures;
 
 public interface MyCollection {
 
- boolean add(Object obj); 
- boolean remove(Object obj);
- boolean contains(Object obj); 
- Object get(int index); 
- Object[] toArray(); 
+	int size();
+
+	boolean add(Object obj);
+
+	boolean remove(Object obj);
+
+	boolean contains(Object obj);
+
+	Object get(int index);
+
+	Object[] toArray();
 
 }
